f4b56e5f7e0a3d9b45e20147ca3877dcdd18e467
Mahmoud Youssef <mahmoud.yusof27@gmail.com>
2021-08-22 23:40:35 +0200

    salma's third assignment

--- BitManipulation.java
+++ BitManipulation.java
@@ -12,4 +12,19 @@ public class BitManipulation {
         assertEquals(31-7, BitHelper.clearBitsThrough(num, 2));
         assertEquals(31-14, BitHelper.clearBitsBetween(num, 1, 3));
     }
+
+    public static int insert(int original, int insertion, int i, int j){
+        original = BitHelper.clearBitsBetween(original, i, j);
+        return original | (insertion << i);
+    }
+
+    @Test
+    public void testInsert(){
+        int original = Integer.parseInt("1010", 16); // 0001 0000 0001 0000
+        int insertion = Integer.parseInt("00F0", 16); // 0000 0000 1111 0000
+        int output = insert(original, insertion, 0, 8); // 0001 0000 1111 0000
+        assertEquals(Integer.parseInt("10F0", 16), output);
+    }
+
+    // 5.5 The code checks if the n is power of two (only one bit is high (one hot))
 }
